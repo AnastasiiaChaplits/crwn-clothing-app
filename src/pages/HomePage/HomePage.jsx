fed80c33e3d14b711b6ea322bb1b7ad15c982672
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-04-05 16:06:30 +0200

    styled MenuItem

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,10 @@
-import './styles.scss';
-import { Directory } from '../../components';
+import "./styles.scss";
+import { Directory } from "../../components";
 
 const HomePage = () => (
-  <div className='homepage'>
+  <div className="homepage">
     <Directory />
   </div>
-)
+);
 
 export default HomePage;
