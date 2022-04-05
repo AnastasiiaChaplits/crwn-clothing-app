c41ee0a654275f7664543ccd294e39864af526f0
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-04-05 15:28:50 +0200

    restructured directories and add styling

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import './styles.scss';
+import { Directory } from '../../components';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+)
+
+export default HomePage;
