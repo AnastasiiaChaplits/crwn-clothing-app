e039bb9bbe37467fd08e54c756632a0253172eb5
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-04-08 15:02:58 +0200

    create  custom button

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { FormInput } from "../index";
+import { FormInput, CustomButton } from "../index";
 import "./styles.scss";
 
 class SignIn extends Component {
@@ -25,7 +25,7 @@ class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
-        <h2>I already have an account</h2>
+        <h2 className="title">I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -45,7 +45,7 @@ class SignIn extends Component {
             required
             handleChange={this.handleChange}
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign in</CustomButton>
         </form>
       </div>
     );
--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as CollectionItem } from "./CollectionItem/CollectionItem";
 export { default as Header } from "./Header/Header";
 export { default as SignIn } from "./SignIn/SignIn";
 export { default as FormInput } from "./FormInput/FormInput";
+export { default as CustomButton } from "./CustomButton/CustomButton";
