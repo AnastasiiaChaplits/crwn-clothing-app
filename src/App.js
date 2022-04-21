67a6606179d10a4029eebba031fce44177cecc9e
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-04-21 16:37:22 +0200

    implemented check is user exist with redux-saga

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -6,29 +7,45 @@ import "./App.css";
 import { HomePage, ShopPage, SignInAndSignUpPage, CheckoutPage } from "./pages";
 import { Header } from "./components";
 import { selectCurrentUser } from "./redux/user/userSelector";
+import { checkUserSession } from "./redux/user/userActions";
 
-const App = ({ currentUser }) => {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route
-          exact
-          path="/signin"
-          render={() =>
-            currentUser ? <Redirect to="" /> : <SignInAndSignUpPage />
-          }
-        />
-      </Switch>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    const { checkUserSession } = this.props;
+    checkUserSession();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+});
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
