23a9b73e160bb83469ba52a2d02a641a9352679e
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-04-27 15:58:22 +0200

    created server

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,4 +1,5 @@
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -6,8 +7,23 @@ const StripeButton = ({ price }) => {
     "pk_test_51I9sKVK6nfHnMUlI6XjGNSfzLcwjGSr8xqbXEYFK3PQBRS6ur9qtRzP2hvaSuyDxzbS8zEuIFALc6BJYIErCYNdz00nJqFbTt9";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please, sure you use provided credit card."
+        );
+      });
   };
 
   return (
